ae41dc1f38fa1e2d1e16ca705ba787c1d880e76d
Leszek Wisniewski <leszek.wisniewski92@gmail.com>
2021-02-20 17:04:29 +0200

    add route to get comics and add dependencies and update docker

--- src/main/java/com/leszek/comicpull/App.java
+++ src/main/java/com/leszek/comicpull/App.java
@@ -1,12 +1,29 @@
 package com.leszek.comicpull;
 
+import com.leszek.comicpull.classes.Comic;
 import io.jooby.Jooby;
+import io.jooby.di.GuiceModule;
+import io.jooby.json.JacksonModule;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class App extends Jooby {
 
   {
+    // add dependency Injection
+    install(new GuiceModule());
+    // add Jackson
+    install(new JacksonModule());
 
     get("/", ctx -> "Welcome to Jooby!");
+
+    get("/comics", ctx -> {
+      List<Comic> comicList = new ArrayList<>();
+      //todo add logic
+      return comicList;
+    });
   }
 
   public static void main(final String[] args) {
